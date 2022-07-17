b6714478918b915ef5bcaae2a80825ace8bf13f0
arinddas <adasgupta60@gmail.com>
2022-07-17 16:11:08 +0530

    small fix

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -2,7 +2,7 @@
 import type { NextPage } from 'next'
 import Head from 'next/head'
 import NextLink from "next/link"
-import { VStack, Heading, Box, LinkOverlay, LinkBox, useColorModeValue} from "@chakra-ui/layout"
+import { VStack, Heading, Box, LinkOverlay, LinkBox} from "@chakra-ui/layout"
 import { Text, Button } from '@chakra-ui/react'
 import { useState, useEffect} from 'react'
 import {ethers} from "ethers"
